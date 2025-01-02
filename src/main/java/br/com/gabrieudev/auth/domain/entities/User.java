3b2eed269a1a45704ecc3e9dce0a645d8c9e53d8
João Gabriel da Conceição Santos <gabrieudev@gmail.com>
2025-01-02 14:00:53 -0300

    feat: create domain entities

---
+++ src/main/java/br/com/gabrieudev/auth/domain/entities/User.java
@@ -0,0 +1,53 @@
+package br.com.gabrieudev.auth.domain.entities;
+
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(UUID id, String firstName, String lastName, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+}
