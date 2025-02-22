1fcb72599bc776e477bc96f73b738edc76a1122b
João Gabriel da Conceição Santos <gabrieudev@gmail.com>
2025-02-22 15:25:57 -0300

    feat: create DTOs

---
+++ src/main/java/br/com/gabrieudev/auth/adapters/input/rest/dtos/auth/RefreshTokenResponse.java
@@ -0,0 +1,13 @@
+package br.com.gabrieudev.auth.adapters.input.rest.dtos.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshTokenResponse {
+    private String accessToken;
+    private String refreshToken;
+}
---
+++ src/main/java/br/com/gabrieudev/auth/adapters/input/rest/dtos/user/UserDTO.java
@@ -0,0 +1,35 @@
+package br.com.gabrieudev.auth.adapters.input.rest.dtos.user;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.modelmapper.ModelMapper;
+
+import br.com.gabrieudev.auth.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Boolean isActive;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static UserDTO from(User user) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(user, UserDTO.class);
+    }
+
+    public User toDomainObj() {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(this, User.class);
+    }
+}
