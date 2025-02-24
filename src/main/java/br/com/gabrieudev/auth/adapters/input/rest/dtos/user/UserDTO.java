cc0dfe0b196f6e3d5f1b63ee9793169b376eef2d
João Gabriel da Conceição Santos <gabrieudev@gmail.com>
2025-02-24 17:58:44 -0300

    fix: remove UserDTO password field

--- src/main/java/br/com/gabrieudev/auth/adapters/input/rest/dtos/user/UserDTO.java
+++ src/main/java/br/com/gabrieudev/auth/adapters/input/rest/dtos/user/UserDTO.java
@@ -18,7 +18,6 @@ public class UserDTO {
     private String firstName;
     private String lastName;
     private String email;
-    private String password;
     private Boolean isActive;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
